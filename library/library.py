09738fe555b6d7116cb04c57f24142d82cfa04dd
JexBest <xxqpxx@mail.ru>
2024-10-23 21:15:17 +0300

    breake autorization

--- library/library.py
+++ library/library.py
@@ -3,33 +3,6 @@ import json
 import os
 
 from auth import auth_user 
-def main_library_function():
-    while True:
-        command = input("Для работы с библиотекой введите: 'добавить', 'изменить', 'удалить', 'просмотр', 'поиск', 'выйти': ").lower()
-        if command == "добавить":
-            добавить_книгу(books)
-        elif command == "просмотр":
-            просмотр_книги(books)
-        elif command == "удалить":
-            удалить_книгу(books)
-        elif command == "изменить":
-            изменить_книгу(books)
-        elif command == "поиск":
-            поиск_книги(books)
-        elif command == "выйти":
-            сохранение_json(books)
-            break
-        else:
-            print("Не корректный ввод, повторите запрос!")
-
-
-# Вызов функции авторизации
-if auth_user():
-    print("Авторизация пройдена, добро пожаловать в библиотеку!")
-    main_library_function()
-else:
-    print("Авторизация не пройдена.")
-
 class Book:
     def __init__(self, name, author, year, genre):
         self.name = name
@@ -41,7 +14,6 @@ class Book:
         print(f"Название: {self.name}, Автор: {self.author}, Год: {self.year}, Жанр: {self.genre}")
 import json
 #wdaawdad
-
 books_dir = "library/books"
 os.makedirs(books_dir, exist_ok=True)
 books_json = os.path.join(books_dir, "books.json")
@@ -249,6 +221,42 @@ def сохранение_json(books, filename=books_json):
 
 
 
-books = чтение_бд()
+
+
+
+
+def main_library_function():
+    while True:
+        command = input("Для работы с библиотекой введите: 'добавить', 'изменить', 'удалить', 'просмотр', 'поиск', 'выйти': ").lower()
+        if command == "добавить":
+            добавить_книгу(books)
+        elif command == "просмотр":
+            просмотр_книги(books)
+        elif command == "удалить":
+            удалить_книгу(books)
+        elif command == "изменить":
+            изменить_книгу(books)
+        elif command == "поиск":
+            поиск_книги(books)
+        elif command == "выйти":
+            сохранение_json(books)
+            break
+        else:
+            print("Не корректный ввод, повторите запрос!")
+
+
+# Вызов функции авторизации
+if auth_user():
+    print("Авторизация пройдена, добро пожаловать в библиотеку!")
+    books = чтение_бд()
+    main_library_function()
+else:
+    print("Авторизация не пройдена.")
+
+
+
+
+
+
 
 
